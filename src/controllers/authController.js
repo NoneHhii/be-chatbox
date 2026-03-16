1044a9e2c8eb84654fbb814d3a64e381d7583dba
NoneHhii <thienkhoatgddqng@gmail.com>
2026-03-16 19:37:06 +0700

    fix login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,11 +32,10 @@ exports.login = async (req, res) => {
     try {
         const {email, password} = req.body;
 
-        const result = userModel.findEmail(email);
+        const result = await userModel.findEmail(email);
+        if(!result || result.rows.length === 0) return res.status(400).json({message: "User not found"});
 
-        if(result.rows.length === 0) return res.status(400).json({message: "User not found!"});
-
-        const user = (await result).rows[0];
+        const user = result.rows[0];
 
         const match = await bcrypt.compare(password, user.password);
 
@@ -49,16 +48,15 @@ exports.login = async (req, res) => {
         );
 
         const {password: _, ...safeUserInfo} = user;
-        res.json({
+        return res.json({
             message: "Login success",
             token,
             user: safeUserInfo
         });
 
-        res.token({token, user});
     } catch (error) {
         console.log(error);
-        res.status(500).json("Login failure!");
+        res.status(500).json({message: "Login failure!", error: error.message});
     }
 }   
 
