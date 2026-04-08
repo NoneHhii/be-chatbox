382f3c011343fa2e7efb0bc5f21aec92e55d186c
NoneHhii <thienkhoatgddqng@gmail.com>
2026-04-08 22:33:31 +0700

    fix mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,28 +1,28 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns'); // Thêm thư viện dns có sẵn của Node.js
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
+    secure: true, // Dùng SSL cho cổng 465
     auth: {
         user: "thienkhoatgddqng@gmail.com",
         pass: process.env.AppPassword 
     },
-    // THÊM ĐOẠN NÀY ĐỂ FIX LỖI ENETUNREACH
-    connectionTimeout: 10000, // 10 giây
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
+    // ĐOẠN QUAN TRỌNG NHẤT: Ép buộc dùng IPv4
     dnsLookup: (hostname, options, callback) => {
-        require('dns').lookup(hostname, { family: 4 }, callback);
+        dns.lookup(hostname, { family: 4 }, (err, address, family) => {
+            callback(err, address, family);
+        });
     }
 });
 
-exports.sendOTP = async(email, otp) => {
+exports.sendOTP = async (email, otp) => {
     const mailOptions = {
-        from: 'ChatBox Team <no-reply@gmail.com>',
+        from: '"ChatBox Team" <thienkhoatgddqng@gmail.com>', // Sửa lại From cho đồng nhất
         to: email,
         subject: 'Mã xác thực tài khoản',
-        text: `Mã OTP của bạn là ${otp}. Có hiệu lực trong 5 phút`
+        text: `Mã OTP của bạn là ${otp}. Có hiệu lực trong 5 phút.`
     };
     return transporter.sendMail(mailOptions);
-}
+};
